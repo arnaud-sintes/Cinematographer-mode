d2d6062d0ec47b44b7a0ce992521f92c24bd4f98
Arnaud SINTES <a.sintes@quantumsurgical.com>
2023-03-16 13:23:29 +0100

    repo creation

---
+++ cinemato/cinemato.c
@@ -0,0 +1,376 @@
+#include <module.h>
+#include <dryos.h>
+#include <menu.h>
+#include <config.h>
+#include <lens.h>
+#include <bmp.h>
+#include <powersave.h>
+
+
+// focus sequence speed entry count:
+#define CINEMATO_FOCUS_SEQUENCE_SPEED_COUNT 5
+
+// focus sequence speed structure definition:
+typedef struct
+{
+    // speed value count:
+    int count;
+    
+    // current index array:
+    int index;
+    
+    // displayed label:
+    const char * const labels[ CINEMATO_FOCUS_SEQUENCE_SPEED_COUNT ];
+    
+    // related sleep data (ms):
+    const int data[ CINEMATO_FOCUS_SEQUENCE_SPEED_COUNT ];
+}
+Cinemato_FocusSequenceSpeed;
+
+
+// focus sequence point definition:
+typedef struct
+{
+    // absolute focus step value:
+    int steps;
+    
+    // transition speed (index):
+    int speedIndex;
+    
+    // lens distance, in mm:
+    int distance;
+}
+Cinemato_FocusSequencePoint;
+
+
+// focus sequence structure definifion:
+typedef struct
+{
+    // sequence length:
+    int length;
+    
+    // current index in sequence:
+    int index;
+    
+    Cinemato_FocusSequenceSpeed speed;
+    
+    // sequence data:
+    Cinemato_FocusSequencePoint data[ 100 ]; // max 100 points in the sequence
+}
+Cinemato_FocusSequence;
+
+
+// cinematographer global data structure definition:
+typedef struct
+{
+    // is the cinematographer task currently running ?
+    bool isTaskRunning;
+    
+    // is the cinematographer mode currently enabled ?
+    bool isEnabled;
+    
+    // are we in PLAY mode ? (or in EDIT mode then)
+    bool isModePlay; 
+    
+    // are we currently doing a focus transition?
+    bool isInTransition;
+    
+    // does the camera screen display currently activated?
+    bool isDisplayOn;
+    
+    // current asbolute focus step value:
+    // note: steps are anyway related to the initial value at camera startup
+    int focusSteps;
+    
+    Cinemato_FocusSequence focusSequence;
+}
+Cinemato_Data;
+
+
+// global data values with default values:
+static Cinemato_Data g_cinemato_data = {
+    .isTaskRunning  = false,
+    .isEnabled      = false,
+    .isModePlay     = true,
+    .isInTransition = false,
+    .isDisplayOn    = true,
+    .focusSteps     = 0,
+    .focusSequence  = {
+        .length         = 0,
+        .index          = 0,
+        .speed          = {
+            .count          = CINEMATO_FOCUS_SEQUENCE_SPEED_COUNT,
+            .index          = 0,
+            .labels         = { "fastest", "fast", "medium", "slow", "slowest" },
+            .data           = { 0, 10, 50, 100, 200 }
+        }
+    }
+};
+
+
+// specific print function:
+static void Cinemato_Print( const char * _fmt, ... )
+{    
+    // unfold arguments regarding format:
+    va_list args;
+    static char data[ 42 ]; // max line length
+    va_start( args, _fmt );
+    int dataLen = sizeof( data );
+    int written = vsnprintf( data, dataLen - 1, _fmt, args );
+    va_end( args );
+    
+    // complement data with spaces regarding line width:
+    memset( data + written, ' ', dataLen - 1 - written );
+    data[ dataLen - 1 ] = 0;
+    
+    // draw label with data, white font over black background:
+    static uint32_t fontspec = FONT( FONT_SMALL, COLOR_WHITE, COLOR_BLACK );
+    int x = 0; // stick on the left
+    int y = 100; // 100px from top
+    bmp_puts( fontspec, &x, &y, data );
+}
+
+
+// overlay task conditional print:
+static void Cinemato_OverlayTask_Print()
+{
+    // notation shortcuts:
+    const Cinemato_Data * const pData = &g_cinemato_data;
+    const Cinemato_FocusSequence * const pFocusSequence = &pData->focusSequence;
+    const Cinemato_FocusSequenceSpeed * const pFocusSequenceSpeed = &pFocusSequence->speed;
+    
+    // cinematographer mode currently disabled, just display:
+    // [idle]
+    if( !pData->isEnabled ) {
+        Cinemato_Print( "[idle]" );
+        return;
+    }
+    
+    // we're in EDIT mode, display the current recorded point count (1),
+    // alongside current lens distance (2) and speed (3) to be queued, under the form:
+    // [edit] (1) < (2)mm (3)
+    if( !pData->isModePlay ) {
+        const int lensDistance = lens_info.focus_dist * 10;
+        const char * const currentSpeed = pFocusSequenceSpeed->labels[ pFocusSequenceSpeed->index ];
+        Cinemato_Print( "[edit] %d < %dmm (%s)", pFocusSequence->length, lensDistance, currentSpeed );
+        return;
+    }
+    
+    // we're in PLAY mode:
+    const int lensDistance = lens_info.focus_dist * 10;
+        
+    // we're not in transition, display the focus lens index (1) over the sequence length (2),
+    // alongside both current lens distance (3), target distance of next reachable point (4)
+    // and transition speed (5), under the form:
+    // [play] (1)/(2) (3)mm > (4)mm (5)
+    if( !pData->isInTransition ) {
+        int targetIndex = pFocusSequence->index + 1;
+        if( targetIndex > pFocusSequence->length )
+            targetIndex = 1;
+        const Cinemato_FocusSequencePoint * const pTargetPoint = &pFocusSequence->data[ targetIndex - 1 ];
+        const char * const targetSpeed = pFocusSequenceSpeed->labels[ pTargetPoint->speedIndex ];
+        Cinemato_Print( "[play] %d/%d %dmm > %dmm (%s)", pFocusSequence->index, pFocusSequence->length, lensDistance, pTargetPoint->distance, targetSpeed );
+        return;
+    }
+    
+    // we're in transition, display the focus lens index (1) source and the focus lens
+    // destination (2) alongside current lens distance (3) and transition speed (4), under the form:
+    // [play] (1)>(2) (3)mm (4)
+    // note: the focus lens index was already incremented at this point
+    int sourceIndex = pFocusSequence->index - 1;
+    if( sourceIndex == 0 )
+        sourceIndex = pFocusSequence->length;
+    const Cinemato_FocusSequencePoint * const pSourcePoint = &pFocusSequence->data[ sourceIndex - 1 ];
+    const char * const sourceSpeed = pFocusSequenceSpeed->labels[ pSourcePoint->speedIndex ];
+    Cinemato_Print( "[play] %d>%d %dmm (%s)", sourceIndex, pFocusSequence->index, lensDistance, sourceSpeed );
+}
+
+
+// overlay task:
+static void Cinemato_OverlayTask()
+{
+    // notation shortcuts:
+    Cinemato_Data * const pData = &g_cinemato_data;
+    
+    // task is now running:
+    pData->isTaskRunning = true;
+    
+    // loop indefinitely in order to deal with cinematographer mode display overlays:
+    for(;;) {
+        Cinemato_OverlayTask_Print();
+        
+        // breathe a little to let other tasks do their job properly:
+        msleep( 100 );
+    }
+}
+
+
+// generic focus update:
+static void Cinemato_UpdateFocus( const int _steps, const int _waitMs )
+{
+    // notation shortcuts:
+    Cinemato_Data * const pData = &g_cinemato_data;
+    
+    // start transition:
+    pData->isInTransition = true;
+    
+    // change lens focus accurately:
+    lens_focus( _steps, 1, 1, _waitMs );
+    
+    // update asbolute focus step value:
+    pData->focusSteps += _steps;
+    
+    // stop transition:
+    pData->isInTransition = false;
+}
+
+
+// menu definition:
+static struct menu_entry g_cinemato_menu[] = { {
+    .name    = "Cinematographer",
+    .select  = run_in_separate_task, // DryOS task, running in parallel
+    .priv    = Cinemato_OverlayTask,
+    .help    = "Start cinematographer mode with focus sequencing",
+} };
+
+
+// key handler:
+static unsigned int Cinemato_KeyHandler( const unsigned int _key )
+{
+    // notation shortcuts:
+    Cinemato_Data * const pData = &g_cinemato_data;
+    Cinemato_FocusSequence * const pFocusSequence = &pData->focusSequence;
+    Cinemato_FocusSequenceSpeed * const pFocusSequenceSpeed = &pFocusSequence->speed;
+    
+    // task is not running, bypass:
+    if( !pData->isTaskRunning )
+        return 1;
+    
+    // [INFO] push button allows to enable/disable cinematographer mode:
+    if( _key == MODULE_KEY_INFO ) {
+        pData->isEnabled =! pData->isEnabled;
+        return 0;
+    }
+    
+    // cinematographer mode is disabled, no need to track other keys:
+    if( !pData->isEnabled )
+        return 1;
+    
+    // quick focus change constant:
+    const int manualQuickFocusChange = 10; // 10 steps movement
+    
+    // custom keys:
+    switch( _key ) {
+        
+        // [UP] push button allows to precisely decrease the lens focus:
+        case MODULE_KEY_PRESS_UP:
+            Cinemato_UpdateFocus( -1, 0 );
+            return 0;
+            
+        // [RIGHT] push button allows to quickly decrease the lens focus:
+        case MODULE_KEY_PRESS_RIGHT:
+            Cinemato_UpdateFocus( -manualQuickFocusChange, 0 );
+            return 0;
+            
+        // [DOWN] push button allows to precisely increase the lens focus:
+        case MODULE_KEY_PRESS_DOWN:
+            Cinemato_UpdateFocus( 1, 0 );
+            return 0;
+            
+        // [LEFT] push button allows to quickly increase the lens focus:
+        case MODULE_KEY_PRESS_LEFT:
+            Cinemato_UpdateFocus( manualQuickFocusChange, 0 );
+            return 0;
+            
+        // [Q] push button allows to toggle between PLAY and EDIT modes:
+        case MODULE_KEY_Q:
+            pData->isModePlay = !pData->isModePlay;
+        
+            // PLAY mode activated, we set the current sequence index at the end of the list:
+            if( pData->isModePlay ) {
+                pFocusSequence->index = pFocusSequence->length;
+                return 0;
+            }
+            
+            // EDIT mode activated, we reset the list length
+            pFocusSequence->length = 0;
+            return 0;
+            
+        // [SET] push button allows to record or replay a sequence points:
+        case MODULE_KEY_PRESS_SET:
+            // we're in EDIT mode, we just need to save the current lens focus information in the list:
+            if( !pData->isModePlay ) {
+                Cinemato_FocusSequencePoint * pPoint = &pFocusSequence->data[ pFocusSequence->length++ ];
+                pPoint->steps = pData->focusSteps;
+                pPoint->speedIndex = pFocusSequenceSpeed->index;
+                pPoint->distance = lens_info.focus_dist * 10;
+                return 0;
+            }
+            
+            // we're in PLAY mode, but the list is empty:
+            if( pFocusSequence->length == 0 )
+                return 0;
+            
+            // we reach the end of the list, jump to the first point:
+            if( ++pFocusSequence->index > pFocusSequence->length )
+                pFocusSequence->index = 1;
+            
+            // do lens focus transition from the current absolute position to the one we're targeting:
+            const Cinemato_FocusSequencePoint * const pPoint = &pFocusSequence->data[ pFocusSequence->index - 1 ];
+            int steps = pPoint->steps - pData->focusSteps;
+            int waitMs = pFocusSequenceSpeed->data[ pPoint->speedIndex ];
+            Cinemato_UpdateFocus( steps, waitMs );
+            return 0;
+            
+        // [RATE] push button allows to change the sequence speed
+        case MODULE_KEY_RATE:
+            // increase the current speed index, loop if we're reaching the end of the list:
+            if( ++pFocusSequenceSpeed->index == CINEMATO_FOCUS_SEQUENCE_SPEED_COUNT )
+                pFocusSequenceSpeed->index = 0;
+            return 0;
+            
+        // [PLAY] push button allows to toggle the camera display on/off:
+        case MODULE_KEY_PLAY:
+            pData->isDisplayOn = !pData->isDisplayOn;
+            
+            // turn on the camera display:
+            if( pData->isDisplayOn ) {
+                display_on();
+                return 0;
+            }
+            
+            // turn off the camera display:
+            display_off();
+            return 0;
+            
+        // bypass by default:
+        default:
+            return 1;
+    }
+}
+
+
+// init:
+static unsigned int Cinemato_Init()
+{
+    // add cinematographer mode to Movie menu:
+    menu_add( "Movie", g_cinemato_menu, COUNT( g_cinemato_menu ) );
+    return 0;
+}
+
+
+// deinit:
+static unsigned int Cinemato_DeInit()
+{
+    return 0;
+}
+
+
+MODULE_INFO_START()
+    MODULE_INIT( Cinemato_Init )
+    MODULE_DEINIT( Cinemato_DeInit )
+MODULE_INFO_END()
+
+MODULE_CBRS_START()
+    MODULE_CBR( CBR_KEYPRESS, Cinemato_KeyHandler, 0 )
+MODULE_CBRS_END()
---
+++ readme.md
@@ -0,0 +1,201 @@
+# Cinematographer mode
+
+## 1. Purpose
+
+*Cinematographer mode* is a [Magic Lantern](https://magiclantern.fm/) **module** helping to solve the following situations:
+
+- We cannot use an external *focus puller monitor* because we're recording videos using **crop modes**, which are currently incompatible with the camera *HDMI output*
+- We're recording videos as a **single camera operator** without the assistance of a *focus puller* and the sequence is too complex to deal with *manual focusing*
+
+👉 *Cinematographer mode* provides then a way to easily **record multiple lens focus points**, so we can prepare a sequence including camera or target movements and subsequently **replay** them as we're recording videos, using a simple push button to switch to a point to the next one in the sequence.
+
+> **Typical use case**: I'm a *single camera operator* recording a video using *crop modes* and my sequence is a combination of a *dolly-in* movement (with a speed ramp) and a horizontal camera rotation used to keep an object at the center of the composition.
+>
+> 👉 While I'm rehearsing the camera track and movements, I'll record three or four intermediate focus points in a sequence with proper speed transition settings, so my targeted object is always perfectly focused and I'll replay the focus sequence while I'm finally recording the scene, going forward from a focus point to the next one by pushing a single button.
+
+## 2. How to build ?
+
+If we want to build the module ourselves, we basically need to follow [these instructions](https://www.magiclantern.fm/forum/index.php?topic=20214.0) if we're under the **Windows 10** operating system, relying over [WSL](https://en.wikipedia.org/wiki/Windows_Subsystem_for_Linux).
+
+We need first to [install a Linux subsystem](https://learn.microsoft.com/fr-fr/windows/wsl/install), which can be done running the following command under *PowerShell*:
+
+```bash
+wsl --install
+```
+
+The first time, we will have to register some credentials and reboot.
+
+To configure the ***Magic Lantern* build environment**, we will have to execute the following commands:
+
+```bash
+sudo apt-get update
+sudo apt-get install make gcc gcc-arm-none-eabi mercurial gcc-mingw-w64 python3-docutils zip
+sudo apt-get install python2
+```
+
+Then we have to *clone* a ***Magic Lantern* repository**, for example [the one maintained by Danne](https://bitbucket.org/Dannephoto/magic-lantern_dannephoto_git/src/master/) containing the code base of the ***crop_rec_4k_mlv_snd_isogain_1x3_presets*** fork of *Magic Lantern*:
+
+```bash
+git clone https://bitbucket.org/Dannephoto/magic-lantern_dannephoto_git.git
+```
+
+To add and build the *cinematographer mode* **module**, we basically need to follow [these instructions](https://www.magiclantern.fm/forum/index.php?topic=19232.0), which can be summarized with the following command sequence:
+
+- Adding a "*cinemato*" module folder with one header and one source files:
+
+```bash
+cd magic-lantern_dannephoto_git/modules/
+mkdir cinemato
+cd cinemato
+touch cinemato.c
+touch module_strings.h
+```
+
+- Copying and altering a *Makefile* from an existing module:
+
+```bash
+cp ../ettr/Makefile .
+sed -i "s/ettr/cinemato/" Makefile
+```
+
+Now we just have to replace both "***cinemato.c***" and "***module_strings.h***" file content with the one provided in the */cinemato/* folder of this *git* repository, then build the module using the following command line:
+
+```bash
+make
+```
+
+If all the planets are well aligned, we must see a "***cinemato.mo***" module file appears in the folder.
+
+## 3. How to install ?
+
+We can use the pre-built "***cinemato.mo***" module provided in the */cinemato/* folder of this *git* repository.
+
+> ⚠ Please note this module was currently only tested on a ***Canon 5D mark 3*** camera with ***1.1.3*** firmware (*5D3.113* target).
+
+To install the module:
+
+- Copy the "***cinemato.mo***" module file to the **camera SD card** in the ***/ML/modules/*** folder
+- Start the camera and enter *Magic Lantern* menu (using the [***trashcan***] button)
+- Go to the "**Modules**" menu and load ([***SET***] button) the **cinematographer** module
+- Re-start the camera
+
+We may have a new "***cinematographer***" option in the *Magic Lantern*'s **Movie** menu.
+
+## 4. How to use ?
+
+To use the *Cinematographer mode*, we just need to **start it** by selecting the "*cinematographer*" option in the *Magic Lantern*'s **Movie** menu.
+
+Then, we may see the following display's **overlay** on the very left side of the screen (*100pixels from the top*):
+
+```
+[idle]
+```
+
+Which is indicating the *Cinematographer mode* is waiting to be enabled.
+
+👆 In order to **enable** (or subsequently **disable**) the *Cinematographer mode*, we just need to press the [***INFO***] button.
+
+> ⚠ Enabling the *Cinematographer mode* will hook a lot of camera keys (like the *joystick*) so we need to think about disabling it **before** going through *Magic Lantern* menus to avoid some inconvenient situations...
+
+When enabled we may see a new overlay content, depending of the current **focus sequence mode** (***PLAY*** by default).
+
+👆 We can toggle between ***PLAY*** and ***EDIT*** modes by pressing the [***Q***] button.
+
+### 4.1. Lens focus change
+
+In both ***PLAY*** and ***EDIT*** *focus sequence modes*, we can use the camera *joystick* in order to change the **lens focus point**:
+
+- 👆 [***JOYSTICK_UP***] button will **slowly** (*step by step*) **decrease** the focus
+- 👆 [***JOYSTICK_DOWN***] button will **slowly** (*step by step*) **increase** the focus
+- 👆 [***JOYSTICK_RIGHT***] button will **quickly** (*step of 10*) **decrease** the focus
+- 👆 [***JOYSTICK_LEFT***] button will **quickly** (*step of 10*) **increase** the focus
+
+> Using *Cinematographer mode* the lens itself must be in **autofocus mode** and we need to always take care of:
+>
+> - never do **manual focus** operations
+> - never doing **autofocus** (*half-press shutter*, in most of the cases)
+>
+> In both cases, *Magic Lantern* will lose its internal relative focus step reference and focus sequencing will be fucked up.
+
+### 4.2. Edit mode
+
+Each time we're switching to ***EDIT*** *focus sequence mode*, the focus sequence itself is **reset** and the camera display overlay will look like this:
+
+```
+[edit] 0 < 450mm (fastest)
+```
+
+Indicating:
+
+- The current **recorded point count** *{1}*
+- The current **lens distance** (mm) *to be queued* *{2}*
+- The current **transition speed** *to be queued* *{3}*
+
+Under the form:
+
+```
+[edit] {1} < {2}mm ({3})
+```
+
+The **lens distance** indication is dynamically updated regarding our **lens focus change** operations, providing an information about the point that will be **queued next in the list**, alongside the **lens focus transition speed** that will be used to go from the previous focus point in the list to this new one.
+
+👆 Use the [***SET***] button to **queue** the current *lens position* and *selected transition speed* in the **focus sequence**.
+
+👆 Using the [***RATE***] button, we can cycle around **transition speed** presets to select one before queuing: possible speed values are currently ***fastest***, ***fast***, ***medium***, ***slow*** and ***slowest***, corresponding respectively to the addition of a *0ms*, *10ms*, *50ms*, *100ms* and *200ms* delay between two focus rotation steps during the focus lens transition.
+
+### 4.3. Play mode
+
+Each time we're switching to ***PLAY*** *focus sequence mode*, the camera display overlay will look like this:
+
+```
+[play] 3/3 200mm > 450mm (fastest)
+```
+
+Indicating:
+
+- The current **focus index** in the sequence *{1}*
+- The current **sequence length** *{2}*
+- The current **lens distance** (mm) *{3}*
+- The **lens distance** (mm) of the **next focus point** in the sequence *{4}*
+- The **transition speed** that will be used to do the transition from current focus point to the next one *{5}*
+
+Under the form:
+
+```
+[play] {1}/{2} {3}mm > {4}mm ({5})
+```
+
+👆 To run the **focus transition** between the current point in the list to the next one, we just need to press the [***SET***] button.
+
+During the transition, the camera display the following overlay:
+
+```
+[play] 1>2 870mm (slow)
+```
+
+Indicating:
+
+- The **source focus index** in the sequence *{1}*
+- The **target focus index** in the sequence *{2}*
+- The current **lens distance** (mm) *{3}* that will dynamically change during transition
+- The current **transition speed** in use *{4}*
+
+Under the form:
+
+```
+[play] {1}>{2} {3}mm ({4})
+```
+
+### 4.4. Battery saver
+
+👆 One additional feature we may want to use is the [***PLAY***] button that allows to quickly toggle the **camera display on** and **off**, disabling it being a huge **battery saver**, particularly when dealing with video in **crop modes** (the *framed* display really drains the battery).
+
+## 5. What's remaining ?
+
+👷 It will be great to be able to **save on disk** the *focus sequence content* and the *current focus index* to reload it when switching off the camera (but it seems we can just save single integers for now, which make this quite complicated to achieve unless I find a way to save & load raw files by myself).
+
+## 6. License and credits
+
+The *Cinematographer mode* module for *Magic Lantern* is provided under the [GPLv3 licensing model](https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+Thanks to *`Audionut11`* (from *r/MagicLantern*), *`WalterSchulz`* & *`?❄? names_are_hard ?❄?`* (from *Magic Lantern discord channel*) and of course *`a1ex`*, *`Danne`*, *`thebilalfakhouri`* plus all the amazing *Magic Lantern* contributors for the hard work!
